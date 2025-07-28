fe0f647b7e316a11a33b10b366bb9f9db1d38941
pablo <pmargreff+test@gmail.com>
2025-07-28 20:23:42 +0200

    feature: implement fixed discount

---
+++ lib/store/discounts/product_fixed_discount.ex
@@ -0,0 +1,44 @@
+defmodule Store.Discounts.ProductFixedDiscount do
+  @moduledoc """
+  Applies a fixed discount to a product item if it has the given product_id.
+
+  The discount is only applied after the product item has passed the minimum quantity.
+  """
+
+  alias Store.{ProductItem, Product}
+  @behaviour Store.Discounts.DiscountBehaviour
+
+  @enforce_keys [:fixed_discount, :product_id, :minimum_quantity]
+  defstruct [:fixed_discount, :product_id, :minimum_quantity]
+
+  @type t :: %__MODULE__{
+          fixed_discount: integer(),
+          product_id: String.t(),
+          minimum_quantity: non_neg_integer()
+        }
+
+  @impl Store.Discounts.DiscountBehaviour
+  def apply(%__MODULE__{} = discount, product_items) do
+    apply_discount(discount, product_items)
+  end
+
+  defp apply_discount(
+         %__MODULE__{product_id: product_id, minimum_quantity: minimum_quantity} = discount,
+         [
+           %ProductItem{product: %Product{id: product_id}, quantity: quantity} = product_item
+           | rest
+         ]
+       )
+       when quantity >= minimum_quantity do
+    discounted_price = product_item.product.price - discount.fixed_discount
+    [%{product_item | discounted_price: discounted_price} | rest]
+  end
+
+  defp apply_discount(_discount, [product_item | rest]) do
+    [product_item | rest]
+  end
+
+  defp apply_discount(_discount, []) do
+    []
+  end
+end
---
+++ test/store/discounts/product_fixed_discount_test.exs
@@ -0,0 +1,96 @@
+defmodule Store.Discounts.ProductFixedDiscountTest do
+  use ExUnit.Case, async: true
+
+  doctest Store.Discounts.ProductFixedDiscount
+
+  alias Store.Discounts.ProductFixedDiscount, as: Discount
+  alias Store.{ProductItem, Product}
+
+  describe "apply/2" do
+    test "applies a fixed discount to a product item" do
+      discount = %Discount{fixed_discount: 10, product_id: "1", minimum_quantity: 1}
+
+      product_items = [
+        %ProductItem{
+          product: %Product{id: "1", price: 100, name: "Apple", quantity: 1},
+          quantity: 1
+        }
+      ]
+
+      assert Discount.apply(discount, product_items) == [
+               %ProductItem{
+                 product: %Product{id: "1", price: 100, name: "Apple", quantity: 1},
+                 quantity: 1,
+                 discounted_price: 90
+               }
+             ]
+    end
+
+    test "does not apply a fixed discount if the product item does not have the given product_id" do
+      discount = %Discount{fixed_discount: 10, product_id: "1", minimum_quantity: 1}
+
+      product_items = [
+        %ProductItem{
+          product: %Product{id: "2", price: 100, name: "Apple", quantity: 1},
+          quantity: 1
+        }
+      ]
+
+      assert Discount.apply(discount, product_items) == product_items
+    end
+
+    test "does not apply a fixed discount if the product item does not reach the minimum quantity" do
+      discount = %Discount{fixed_discount: 10, product_id: "1", minimum_quantity: 2}
+
+      product_items = [
+        %ProductItem{
+          product: %Product{id: "1", price: 100, name: "Apple", quantity: 1},
+          quantity: 1
+        }
+      ]
+
+      assert Discount.apply(discount, product_items) == [
+               %ProductItem{
+                 product: %Product{id: "1", price: 100, name: "Apple", quantity: 1},
+                 quantity: 1,
+                 discounted_price: nil
+               }
+             ]
+    end
+
+    test "applies a fixed discount to the correct product item for multiple product items" do
+      discount = %Discount{fixed_discount: 10, product_id: "1", minimum_quantity: 1}
+
+      product_items = [
+        %ProductItem{
+          product: %Product{id: "1", price: 100, name: "Apple", quantity: 1},
+          quantity: 1
+        },
+        %ProductItem{
+          product: %Product{id: "2", price: 200, name: "Banana", quantity: 1},
+          quantity: 1
+        }
+      ]
+
+      assert Discount.apply(discount, product_items) == [
+               %ProductItem{
+                 product: %Product{id: "1", price: 100, name: "Apple", quantity: 1},
+                 quantity: 1,
+                 discounted_price: 90
+               },
+               %ProductItem{
+                 product: %Product{id: "2", price: 200, name: "Banana", quantity: 1},
+                 quantity: 1,
+                 discounted_price: nil
+               }
+             ]
+    end
+
+    test "does nothing to empty product items" do
+      discount = %Discount{fixed_discount: 10, product_id: "1", minimum_quantity: 1}
+      product_items = []
+
+      assert Discount.apply(discount, product_items) == []
+    end
+  end
+end
